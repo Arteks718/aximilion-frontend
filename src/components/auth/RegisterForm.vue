18df597b5c5a632bd84b0447e02bc68a4d8d772b
Arteks718 <artexbro.official@gmail.com>
2026-05-07 13:20:52 +0300

    feat: remove opportunity to authorize with facebook

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -75,13 +75,6 @@
         </svg>
         <span class="text-sm font-semibold text-brand-dark">Continue with Google</span>
       </button>
-
-      <button @click="loginWithFacebook" type="button" class="flex w-full items-center justify-center px-4 py-3 bg-white border border-[#bbcabf]/30 rounded-full hover:bg-gray-50 transition-colors space-x-3">
-        <svg class="w-5 h-5" viewBox="0 0 24 24" fill="#1877F2">
-          <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.469h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.469h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
-        </svg>
-        <span class="text-sm font-semibold text-brand-dark">Continue with Facebook</span>
-      </button>
     </div>
   </form>
 </template>
@@ -121,9 +114,5 @@ const onSubmit = handleSubmit(async (values) => {
 
 const loginWithGoogle = () => {
   supabase.auth.signInWithOAuth({ provider: 'google' });
-};
-
-const loginWithFacebook = () => {
-  supabase.auth.signInWithOAuth({ provider: 'facebook' });
 };
 </script>
